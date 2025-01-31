e9cdd8b3f6bc42758ee5246627527819c0c85e9e
Gidbug <oyerindegideon001@gmail.com>
2025-01-31 21:47:50 +0100

    fix: deployment

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ def get_info():
         "slack_name": "YourSlackName",  # Replace with your actual Slack name
         "email": "oyerindegideon01@gmail.com",  # Replace with your HNG registered email
         "current_datetime": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),  # Dynamic UTC time
-        "github_url": "https://github.com/Gidbug/stage-0-project"  # GitHub repo URL
+        "github_url": "https://github.com/Gidbug/HNG-stage-0-backend-project"  # GitHub repo URL
     }
     return jsonify(response), 200
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Allows external access when deployed
 
