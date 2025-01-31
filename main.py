becd2ca29f7cdfd48616f2ee14e3f72a5d922e72
Gidbug <oyerindegideon001@gmail.com>
2025-01-31 22:14:18 +0100

    update port with env variables

--- main.py
+++ main.py
@@ -15,5 +15,6 @@ def get_info():
     }
     return jsonify(response), 200
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Allows external access when deployed
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)  # Allows external access when deployed
 
